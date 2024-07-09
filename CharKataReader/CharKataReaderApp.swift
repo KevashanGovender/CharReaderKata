f5cc988df49a26b9bda2c1cddffdcdacf6966847
Kevashan Govender <23380129+KevashanGovender@users.noreply.github.com>
2024-07-09 21:40:59 +0200

    Initial Commit

---
+++ CharKataReader/CharKataReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharKataReaderApp.swift
+//  CharKataReader
+//
+//  Created by Kevashan Govender on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct CharKataReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
